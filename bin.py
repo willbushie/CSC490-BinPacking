77962a8103832a50487b33a494c58902333a769b
William Bushie <willbushie@gmail.com>
2022-04-12 21:02:49 -0500

    adding a coordinate creation method - only to be used by the bin class

--- bin.py
+++ bin.py
@@ -8,27 +8,16 @@ class bin:
         self.width = 10
         self.totalVolume = self.height * self.length * self.width
         self.usableVolume = self.totalVolume
+        self.contains = []
         self.strips = [
             {
                 "height":self.height,
                 "length":self.length,
                 "width":self.width,
                 "volume":(self.height * self.length * self.width),
-                "coordinates":[
-                    {
-                        "a":{"x":0,"y":0,"z":0},
-                        "b":{"x":0,"y":0,"z":10},
-                        "c":{"x":10,"y":0,"z":10},
-                        "d":{"x":10,"y":0,"z":0},
-                        "e":{"x":0,"y":10,"z":0},
-                        "f":{"x":0,"y":10,"z":10},
-                        "g":{"x":10,"y":10,"z":10},
-                        "h":{"x":10,"y":10,"z":0},
-                    }
-                ]
+                "coordinates":self.createCoordinates(self.height,self.length,self.width)
             }
         ]
-        self.contains = []
 
     # find the appropiate section
     def fillStrip(self,dimensions):
@@ -40,7 +29,21 @@ class bin:
         for i in range(len(self.strips)):
             if self.strips[i]["volume"] <= volume:
                 if max([dimensions["height"], dimensions["length"], dimensions["width"]]) <= max([self.strips[i]["length"],self.strips[i]["height"],self.strips[i]["width"]]):
-                    
+                    pass
         # update self.stips with new strips
         # sort self.strips from smallest to largest on volume
         # based on if the method completed successfully (found and placed a strip) or not, return true or false
+
+    # create the coordinate system based on dimensions
+    def createCoordinates(self,height,length,width):
+        coordinates = {
+            "a":{"x":0,"y":0,"z":0},
+            "b":{"x":0,"y":0,"z":height},
+            "c":{"x":length,"y":0,"z":height},
+            "d":{"x":length,"y":0,"z":0},
+            "e":{"x":0,"y":width,"z":0},
+            "f":{"x":0,"y":width,"z":height},
+            "g":{"x":length,"y":width,"z":height},
+            "h":{"x":length,"y":width,"z":0},
+        }
+        return coordinates

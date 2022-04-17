3579afe63351447639dfc3c498d429a78aa1ed30
William Bushie <willbushie@gmail.com>
2022-04-17 18:18:48 -0500

    bin.show3D() seems to be working as expected

--- bin.py
+++ bin.py
@@ -1,6 +1,15 @@
 # imports
+from operator import xor
+from cv2 import reduce
+from matplotlib import projections
 from box import box
 import json
+
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import product, combinations
+from mpl_toolkits.mplot3d import Axes3D
+from functools import reduce
 
 # this is the bin class
 class bin:
@@ -77,9 +86,21 @@ class bin:
                                         "g":{"x":(self.strips[i]["coordinates"]["e"]["x"] + boxCoordinates["g"]["x"]),"y":self.strips[i]["coordinates"]["e"]["y"],"z":(self.strips[i]["coordinates"]["e"]["z"] + boxCoordinates["g"]["z"])},
                                         "h":{"x":(self.strips[i]["coordinates"]["e"]["x"] + boxCoordinates["h"]["x"]),"y":self.strips[i]["coordinates"]["e"]["y"],"z":self.strips[i]["coordinates"]["e"]["z"]}
                                     }
-                                    self.strips.pop(i)
-                                    box.placed = True
+                                    # setting coordinates of the box after placement
+                                    updatedBoxCoordinates = {
+                                        "a":{"x":self.strips[i]["coordinates"]["a"]["x"],"y":self.strips[i]["coordinates"]["a"]["y"],"z":self.strips[i]["coordinates"]["a"]["z"]},
+                                        "b":{"x":self.strips[i]["coordinates"]["a"]["x"],"y":self.strips[i]["coordinates"]["a"]["y"],"z":(self.strips[i]["coordinates"]["a"]["z"] + boxCoordinates["g"]["z"])},
+                                        "c":{"x":(self.strips[i]["coordinates"]["a"]["x"] + boxCoordinates["g"]["x"]),"y":self.strips[i]["coordinates"]["a"]["y"],"z":(self.strips[i]["coordinates"]["a"]["z"] + boxCoordinates["g"]["z"])},
+                                        "d":{"x":(self.strips[i]["coordinates"]["a"]["x"] + boxCoordinates["g"]["x"]),"y":self.strips[i]["coordinates"]["a"]["y"],"z":self.strips[i]["coordinates"]["a"]["z"]},
+                                        "e":{"x":self.strips[i]["coordinates"]["a"]["x"],"y":(self.strips[i]["coordinates"]["a"]["y"] + boxCoordinates["g"]["y"]),"z":self.strips[i]["coordinates"]["a"]["z"]},
+                                        "f":{"x":self.strips[i]["coordinates"]["a"]["x"],"y":(self.strips[i]["coordinates"]["a"]["y"] + boxCoordinates["g"]["y"]),"z":(self.strips[i]["coordinates"]["a"]["z"] + boxCoordinates["g"]["z"])},
+                                        "g":{"x":(self.strips[i]["coordinates"]["a"]["x"] + boxCoordinates["g"]["x"]),"y":(self.strips[i]["coordinates"]["a"]["y"] + boxCoordinates["g"]["y"]),"z":(self.strips[i]["coordinates"]["a"]["z"] + boxCoordinates["g"]["z"])},
+                                        "h":{"x":(self.strips[i]["coordinates"]["a"]["x"] + boxCoordinates["g"]["x"]),"y":(self.strips[i]["coordinates"]["a"]["y"] + boxCoordinates["g"]["y"]),"z":self.strips[i]["coordinates"]["a"]["z"]}
+                                    }
+                                    box.coordinates = updatedBoxCoordinates
                                     self.contains.append(box)
+                                    box.placed = True
+                                    self.strips.pop(i)
                                     stripA = self.createStrip(stripACoordinates)
                                     stripB = self.createStrip(stripBCoordinates)
                                     stripC = self.createStrip(stripCCoordinates)
@@ -143,9 +164,95 @@ class bin:
             #pretty = json.dumps(self.strips[i]['coordinates'], indent=4)
             #print(pretty)
 
-    def show3D(self):
+    def show3D(self,title):
         """
         This method attempts to show a 3 dimensional view of the filled spaces inside the bin.\n
         This method currently does not work.
         """
-        pass
+        """ # Create axis
+        x,y,z = np.indices((9,6,12)) #5,4,3
+        boxA = (x < 4) & (y < 3) & (z < 5)
+        boxB = (x < 4) & (y > 2) & (z < 5)
+        # Create Data
+        voxelarray = boxA | boxB
+        # Controll Tranperency
+        alpha = 0.5
+        # Control color
+        colors = np.empty(voxelarray.shape, dtype=object)
+        #colors = np.empty(voxelarray.shape, dtype=object)
+        colors[boxA] = "red"
+        colors[boxB] = "blue"
+        # Plot figure
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        # Voxels is used to customizations of the
+        ax.voxels(voxelarray, facecolors=colors, edgecolor='k')
+        plt.show() """
+
+        """ # wireframes of two boxes one inside the other
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.set_aspect("auto")
+        ax.set_autoscale_on(True)
+        r = [-10, 10]
+        for s, e in combinations(np.array(list(product(r,r,r))), 2):
+            if np.sum(np.abs(s-e)) == r[1]-r[0]:
+                ax.plot3D(*zip(s,e), color="b")
+        #ax.scatter([0],[0],[0],color="g",s=100)
+        d = [-2, 2]
+        for s, e in combinations(np.array(list(product(d,d,d))), 2):
+            if np.sum(np.abs(s-e)) == d[1]-d[0]:
+                ax.plot3D(*zip(s,e), color="g")
+        plt.show() """
+
+        # prepare some coordinates
+        x, y, z = np.indices((9, 6, 12))
+        # creation of transparent bin bounds
+        transparentBin = (x < self.length) & (y < self.width) & (z < self.height)
+        # loop through the strips and create extra
+        if len(self.contains) > 1:
+            boxes = []
+            for i in range(len(self.contains)):
+                boxCoordinates = self.contains[i].returnCoordinates()
+                xResult = (x >= boxCoordinates["a"]["x"]) & (x < boxCoordinates["g"]["x"])
+                yResult = (y >= boxCoordinates["a"]["y"]) & (y < boxCoordinates["g"]["y"])
+                zResult = ((z >= boxCoordinates["a"]["z"]) & (z < boxCoordinates["g"]["z"]))
+                section = (xResult) & (yResult) & (zResult)
+                boxes.append(section)
+            voxelsFinal = reduce(lambda x, y: x | y, boxes)
+        elif len(self.contains) == 1:
+            boxCoordinates = self.contains[0].returnCoordinates()
+            xResult = (x >= boxCoordinates["a"]["x"]) & (x < boxCoordinates["g"]["x"])
+            yResult = (y >= boxCoordinates["a"]["y"]) & (y < boxCoordinates["g"]["y"])
+            zResult = ((z >= boxCoordinates["a"]["z"]) & (z < boxCoordinates["g"]["z"]))
+            section = (xResult) & (yResult) & (zResult)
+            voxelsFinal = section
+        #boxCoordinatesA = self.contains[0].returnCoordinates()
+        #boxCoordinatesB = self.contains[1].returnCoordinates()
+        #opaque_cubes_1 = (x >= boxCoordinatesA["a"]["x"]) & (x < boxCoordinatesA["g"]["x"]) & (y >= boxCoordinatesA["a"]["y"]) & (y < boxCoordinatesA["g"]["y"]) & ((z >= boxCoordinatesA["a"]["z"]) & (z < boxCoordinatesA["g"]["z"]))
+        #opaque_cubes_2 = (x >= boxCoordinatesB["a"]["x"]) & (x < boxCoordinatesB["g"]["x"]) & (y >= boxCoordinatesB["a"]["y"]) & (y < boxCoordinatesB["g"]["y"]) & ((z >= boxCoordinatesB["a"]["z"]) & (z < boxCoordinatesB["g"]["z"])) #((z >= 2) & (z < 7))
+        #opaque_cubes_1 = (x - self.contains[0].coordinates["a"]["x"] < 9) & (y - self.contains[0].coordinates["a"]["y"] < 2) & (z - self.contains[0].coordinates["a"]["z"] < 2)
+        #opaque_cubes_2 = (x - self.contains[1].coordinates["a"]["x"] < 4) & (y - self.contains[1].coordinates["a"]["y"] < 3) & (z - self.contains[1].coordinates["a"]["z"] < 7)
+        #voxelsFinal = opaque_cubes_1 | opaque_cubes_2
+        # combine the objects into a single boolean array
+        baseBin = transparentBin
+        # and plot everything
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        ax.voxels(baseBin, facecolors='white', edgecolor='none', alpha=0.4)
+        if len(self.contains) > 0:
+            ax.voxels(voxelsFinal, facecolors='red', edgecolor='black', alpha=0.7)
+        plt.show()
+
+
+#test = bin(12,9,6)
+#test.fillStrip(box(2,9,2))
+#test.fillStrip(box(5,4,3))
+#test.fillStrip(box(3,3,3))
+#test.fillStrip(box(5,4,3))
+#test.fillStrip(box(3,3,3))
+#test.fillStrip(box(5,4,3))
+#test.fillStrip(box(5,4,3))
+#for b in range(len(test.contains)):
+#    print(f"coordinates: {test.contains[b].coordinates}")
+#test.show3D("testing graph")
--- box.py
+++ box.py
@@ -5,6 +5,7 @@ class box:
         self.length = length
         self.width = width
         self.volume = self.height * self.length * self.width
+        self.coordinates = self.createCoordinates()
     
     def display(self):
         """
@@ -17,3 +18,22 @@ class box:
         This method returns the height, width, and length in a dictionary.
         """
         return {"height":self.height,"length":self.length,"width":self.width}
+
+    def createCoordinates(self):
+        """
+        Create the coordinate system based on dimensions. These dimensions need to be entered in as listed in the method, or the coordinates will be wrong.
+        """
+        coordinates = {
+            "a":{"x":0,"y":0,"z":0},
+            "b":{"x":0,"y":0,"z":self.height},
+            "c":{"x":self.length,"y":0,"z":self.height},
+            "d":{"x":self.length,"y":0,"z":0},
+            "e":{"x":0,"y":self.width,"z":0},
+            "f":{"x":0,"y":self.width,"z":self.height},
+            "g":{"x":self.length,"y":self.width,"z":self.height},
+            "h":{"x":self.length,"y":self.width,"z":0}
+        }
+        return coordinates
+
+    def returnCoordinates(self):
+        return self.coordinates
